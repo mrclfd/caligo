a48e3d310e48d28838c9effbb4432247be308296
Marcel Faraday <mrclfd@yahoo.com>
2024-03-14 11:54:13 +0700

    feat(fun): fixed

--- caligo/modules/fun.py
+++ caligo/modules/fun.py
@@ -8,26 +8,26 @@ from caligo import command, module
 class Profiles(module.Module):
     name: ClassVar[str] = "Funny"
 
-    @command.desc("Menyala abangku🔥🙌")
+    @command.desc("")
     @command.usage(
         ".menyala"
     )
     @command.alias("mnyl")
     async def cmd_menyala(self, ctx: command.Context):  
-        await ctx.respond("")
-        #await asyncio.sleep(1)
+        await ctx.respond("Menyala abangku🔥🙌")
+        await asyncio.sleep(1)
         await ctx.respond("siap panutan🆙✊🙌")
-        #await asyncio.sleep(1)
+        await asyncio.sleep(1)
         await ctx.respond("tipis tipis abangda🦅🎣")
-        #await asyncio.sleep(1)
+        await asyncio.sleep(1)
         await ctx.respond("tetap ilmu padi abangkuh🌾")
-        #await asyncio.sleep(1)
+        await asyncio.sleep(1)
         await ctx.respond("tetap rispek🫡🙌")
-        #await asyncio.sleep(1)
+        await asyncio.sleep(1)
         await ctx.respond("kelas bangda👍🔥")
-        #await asyncio.sleep(1)
+        await asyncio.sleep(1)
         await ctx.respond("kasi paham bos")
-        #await asyncio.sleep(1)
+        await asyncio.sleep(1)
 
         return f"Menyala abangku🔥🙌"
 
