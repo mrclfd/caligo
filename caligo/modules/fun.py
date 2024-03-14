9a50232e3177ef97de6fb30a59a899136a73268d
Marcel Faraday <mrclfd@yahoo.com>
2024-03-14 11:30:27 +0700

    Update fun.py

--- caligo/modules/fun.py
+++ caligo/modules/fun.py
@@ -9,9 +9,9 @@ from caligo import command, module, util
     @command.desc("Kocok")
     async def cmd_kocok(self, ctx: command.Context):
         
-        await ctx.respond("1")
-        await ctx.respond("2")
-        await ctx.respond("3")
+        await ctx.msg.edit("1")
+        await ctx.msg.edit("2")
+        await ctx.msg.edit("3")
 
         return f"Terkocok!"
 
