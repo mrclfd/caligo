aabdc7a81c1a9e3db2ec6e83a52c569f54852e37
Marcel Faraday <mrclfd@yahoo.com>
2024-03-14 11:48:16 +0700

    feat(fun): complete the implementation
    Furthermore, the program code will be made more efficient

--- caligo/modules/fun.py
+++ caligo/modules/fun.py
@@ -1,3 +1,4 @@
+import asyncio
 
 from typing import ClassVar, Optional
 
@@ -7,16 +8,26 @@ from caligo import command, module
 class Profiles(module.Module):
     name: ClassVar[str] = "Funny"
 
-    @command.desc("Kocok")
+    @command.desc("Menyala abangku🔥🙌")
     @command.usage(
-        ".kocok"
+        ".menyala"
     )
-    @command.alias("kc")
-    async def cmd_kocok(self, ctx: command.Context):  
-        await ctx.respond("1")
-        await ctx.respond("2")
-        await ctx.respond("3")
-        await ctx.respond("4")
+    @command.alias("mnyl")
+    async def cmd_menyala(self, ctx: command.Context):  
+        await ctx.respond("")
+        await asyncio.sleep(1)
+        await ctx.respond("siap panutan🆙✊🙌")
+        await asyncio.sleep(1)
+        await ctx.respond("tipis tipis abangda🦅🎣")
+        await asyncio.sleep(1)
+        await ctx.respond("tetap ilmu padi abangkuh🌾")
+        await asyncio.sleep(1)
+        await ctx.respond("tetap rispek🫡🙌")
+        await asyncio.sleep(1)
+        await ctx.respond("kelas bangda👍🔥")
+        await asyncio.sleep(1)
+        await ctx.respond("kasi paham bos")
+        await asyncio.sleep(1)
 
-        return f"Terkocok!!"
+        return f"Menyala abangku🔥🙌"
 
